8c8855b396863207355b7cc357b02ea576f571e6
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-12-06 23:01:51 +0500

    added create post fn

--- fyp_hub/lib/services/marketplace_service.dart
+++ fyp_hub/lib/services/marketplace_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fyp_hub/models/marketplace_post.dart';
 import '../models/supervisor.dart';
 
 class MarketplaceService {
@@ -18,4 +19,11 @@ class MarketplaceService {
   }
 
   // (We will add methods for Projects and Teammates here later)
+  // Add this inside your MarketplaceService class
+  Future<void> createPost(MarketplacePost post) async {
+    await _db
+        .collection('marketplacePosts')
+        .doc(post.postId)
+        .set(post.toJson());
+  }
 }
